ef4f708dadf39085d29302530ef83b810e47349c
Pablobel <pablo.beltran.reyfernando@gmail.com>
2024-03-08 00:30:25 +0100

    Cambio de color random conseguido.

--- app/src/main/java/com/example/pmdm_pablo_beltran/MainActivity.kt
+++ app/src/main/java/com/example/pmdm_pablo_beltran/MainActivity.kt
@@ -7,15 +7,23 @@ import android.graphics.Color
 import android.widget.Button
 import android.widget.EditText
 import android.widget.FrameLayout
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fun getRandomColor(): String {
+            val r = Random.nextInt(256)
+            val g = Random.nextInt(256)
+            val b = Random.nextInt(256)
+            return String.format("#%02X%02X%02X", r, g, b)
+        }
+
         val btnCambiarColor = findViewById<Button>(R.id.btnCambiarColor)
         btnCambiarColor.setOnClickListener {
-            cambiarColorCuerpo("#00FF00") // Ejemplo con verde
+            cambiarColorCuerpo(getRandomColor()) // Ejemplo con verde
         }
 
         val btnCambiarActivity = findViewById<Button>(R.id.btnCambiarActivity)
