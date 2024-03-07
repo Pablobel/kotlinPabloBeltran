f9b7065e1f95ad09c7c84ca1e350bcdc0c1f7ad6
Pablobel <pablo.beltran.reyfernando@gmail.com>
2024-03-08 00:36:14 +0100

    Cambio de color random conseguido.

--- app/src/main/java/com/example/pmdm_pablo_beltran/Activity2.kt
+++ app/src/main/java/com/example/pmdm_pablo_beltran/Activity2.kt
@@ -1,7 +1,9 @@
 package com.example.pmdm_pablo_beltran
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class Activity2 : AppCompatActivity() {
@@ -11,5 +13,13 @@ class Activity2 : AppCompatActivity() {
 
         val mensaje = intent.getStringExtra("mensaje")
         findViewById<TextView>(R.id.textMensaje).text = mensaje
+
+        val btnVolverMainActivity = findViewById<Button>(R.id.btnVolverMainActivity)
+
+        btnVolverMainActivity.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
